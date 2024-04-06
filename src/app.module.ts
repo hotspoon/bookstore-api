50e6e07683fa71353b3c01ae1137a84e256e6f9e
hotspoon <fariez18@outlook.com>
2024-04-06 22:00:06 +0700

    Add AppController and AppService to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { CustomersModule } from './customers/customers.module';
 import { DatabaseModule } from './database/database.module';
 import { BooksModule } from './books/books.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [CustomersModule, DatabaseModule, BooksModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
