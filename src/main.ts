4e68f547348c9da53bf3fc6b5548d58d3aba765a
hotspoon <fariez18@outlook.com>
2024-04-07 14:55:14 +0700

    Update Swagger UI dependencies and configuration in package.json and main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { resolve } from 'path';
 import { writeFileSync, existsSync, readFileSync } from 'fs';
+import * as swaggerUiExpress from 'swagger-ui-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,8 +15,7 @@ async function bootstrap() {
   const pathToSwaggerStaticFolder = resolve(process.cwd(), 'swagger-static');
   const pathToSwaggerJson = resolve(pathToSwaggerStaticFolder, 'swagger.json');
 
-  const swaggerPath = '/api-docs';
-  const swaggerCDN = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.7.2';
+  const swaggerPath = '/swagger-ui';
 
   if (process.env.NODE_ENV === 'development') {
     const config = new DocumentBuilder()
@@ -24,13 +24,7 @@ async function bootstrap() {
       .setVersion('1.0')
       .build();
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup(swaggerPath, app, document, {
-      customCssUrl: swaggerCDN + '/swagger-ui.css',
-      customJs: [
-        swaggerCDN + '/swagger-ui-bundle.js',
-        swaggerCDN + '/swagger-ui-standalone-preset.js',
-      ],
-    });
+    SwaggerModule.setup(swaggerPath, app, document);
 
     // write swagger json file
     const swaggerJson = JSON.stringify(document, null, 2);
@@ -40,13 +34,11 @@ async function bootstrap() {
     // serve static files in production
     if (existsSync(pathToSwaggerJson)) {
       const document = JSON.parse(readFileSync(pathToSwaggerJson, 'utf8'));
-      SwaggerModule.setup(swaggerPath, app, document, {
-        customCssUrl: swaggerCDN + '/swagger-ui.css',
-        customJs: [
-          swaggerCDN + '/swagger-ui-bundle.js',
-          swaggerCDN + '/swagger-ui-standalone-preset.js',
-        ],
-      });
+      app.use(
+        swaggerPath,
+        swaggerUiExpress.serve,
+        swaggerUiExpress.setup(document),
+      );
     }
   }
 
