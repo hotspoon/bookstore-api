02830d3fc746e94db1bedb60f5f4d035f2bb0979
hotspoon <fariez18@outlook.com>
2024-04-07 14:30:33 +0700

    Refactor main.ts to include Swagger UI configuration and use CDN for static file serving

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,6 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { resolve } from 'path';
 import { writeFileSync, existsSync, readFileSync } from 'fs';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { Module } from '@nestjs/common';
-
-const swaggerUiAssetPath = require('swagger-ui-dist').getAbsoluteFSPath();
-
-@Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: swaggerUiAssetPath,
-      serveRoot: '/swagger-ui',
-    }),
-  ],
-})
-class SwaggerStaticModule {}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -29,6 +15,7 @@ async function bootstrap() {
   const pathToSwaggerJson = resolve(pathToSwaggerStaticFolder, 'swagger.json');
 
   const swaggerPath = '/swagger-ui';
+  const swaggerCDN = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.7.2';
 
   if (process.env.NODE_ENV === 'development') {
     const config = new DocumentBuilder()
@@ -37,7 +24,13 @@ async function bootstrap() {
       .setVersion('1.0')
       .build();
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup(swaggerPath, app, document);
+    SwaggerModule.setup(swaggerPath, app, document, {
+      customCssUrl: swaggerCDN + '/swagger-ui.css',
+      customJs: [
+        swaggerCDN + '/swagger-ui-bundle.js',
+        swaggerCDN + '/swagger-ui-standalone-preset.js',
+      ],
+    });
 
     // write swagger json file
     const swaggerJson = JSON.stringify(document, null, 2);
@@ -47,7 +40,13 @@ async function bootstrap() {
     // serve static files in production
     if (existsSync(pathToSwaggerJson)) {
       const document = JSON.parse(readFileSync(pathToSwaggerJson, 'utf8'));
-      SwaggerModule.setup(swaggerPath, app, document);
+      SwaggerModule.setup(swaggerPath, app, document, {
+        customCss: swaggerCDN + '/swagger-ui.css',
+        customJs: [
+          swaggerCDN + '/swagger-ui-bundle.js',
+          swaggerCDN + '/swagger-ui-standalone-preset.js',
+        ],
+      });
     }
   }
 
