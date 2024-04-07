14a5fa6814e9a4d28b1274a9f57497ec094122a2
hotspoon <fariez18@outlook.com>
2024-04-07 13:47:25 +0700

    Update dependencies and configure Swagger UI in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-// main.ts
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { resolve } from 'path';
-import { writeFileSync, existsSync, readFileSync } from 'fs';
-import { static as expressStatic } from 'express';
+import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,9 +10,22 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
 
-  const pathToSwaggerStaticFolder = resolve(process.cwd(), 'swagger-static');
-  const pathToSwaggerJson = resolve(pathToSwaggerStaticFolder, 'swagger.json');
+  app.enableCors({
+    // origin: [
+    //   'https://bookstore-api-ecru.vercel.app',
+    //   'http://localhost:3000',
+    //   'https://bookstore-next-app.vercel.app',
+    // ],
+    origin: '*',
+    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+    credentials: true,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+  });
 
+  await app.listen(process.env.PORT || 3000);
+
+  // get the swagger json file (if app is running in development mode)
   if (process.env.NODE_ENV === 'development') {
     const config = new DocumentBuilder()
       .setTitle('Bookstore API')
@@ -24,27 +35,16 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api-docs', app, document);
 
+    const pathToSwaggerStaticFolder = resolve(process.cwd(), 'swagger-static');
+
     // write swagger json file
+    const pathToSwaggerJson = resolve(
+      pathToSwaggerStaticFolder,
+      'swagger.json',
+    );
     const swaggerJson = JSON.stringify(document, null, 2);
     writeFileSync(pathToSwaggerJson, swaggerJson);
     console.log(`Swagger JSON file written to: '/swagger-static/swagger.json'`);
-  } else if (process.env.NODE_ENV === 'production') {
-    // serve static files in production
-    if (existsSync(pathToSwaggerJson)) {
-      const document = JSON.parse(readFileSync(pathToSwaggerJson, 'utf8'));
-      SwaggerModule.setup('api-docs', app, document);
-    }
   }
-
-  app.enableCors({
-    origin: '*',
-    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
-    credentials: true,
-    allowedHeaders:
-      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
-  });
-
-  await app.listen(process.env.PORT || 3000);
 }
-
 bootstrap();
